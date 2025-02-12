767cb96a67f0bd476d09ceba389e9f02b6fc935e
AnkithMall <ankithmall1729@gmail.com>
2025-02-12 10:15:58 +0530

    iframe position modified

--- dist/assets/index.es.js
+++ dist/assets/index.es.js
@@ -1,25 +1,27 @@
-const h = ({
-  account_id: n,
+const m = ({
+  account_id: s,
   asst_id: l,
-  params: i = {},
-  width: r,
+  params: r = {},
+  width: i,
   height: c,
-  env: a
+  env: d
 }) => {
-  if (n && l) {
-    const o = `${a === "production" ? "https://dashboard.dacoidchat.com" : a === "local" ? "http://localhost:5173" : "https://chatbot-frontend-i8ao.vercel.app"}/embed/${n}/${l}`, p = (t) => Object.keys(t).map((d) => `${encodeURIComponent(d)}=${encodeURIComponent(t[d])}`).join("&");
-    let s;
-    if (Object.keys(i).length > 0)
-      s = `${o}?${p(i)}`;
+  if (s && l) {
+    const o = `${d === "production" ? "https://dashboard.dacoidchat.com" : d === "local" ? "http://localhost:5173" : "https://chatbot-frontend-i8ao.vercel.app"}/embed/${s}/${l}`, p = (t) => Object.keys(t).map((a) => `${encodeURIComponent(a)}=${encodeURIComponent(t[a])}`).join("&");
+    let n;
+    if (Object.keys(r).length > 0)
+      n = `${o}?${p(r)}`;
     else {
       const t = window.location.search;
-      s = t ? `${o}${t}` : o;
+      n = t ? `${o}${t}` : o;
     }
     const e = document.createElement("iframe");
-    e.src = s, e.style.zIndex = "100", e.style.border = "0", e.style.overflowY = "auto", window.matchMedia("(min-width: 1024px)").matches ? (e.style.position = "fixed", e.style.bottom = "10px", e.style.right = "10px", e.style.width = r ? `${r}` : "360px", e.style.height = c ? `${c}` : "700px") : (e.style.position = "fixed", e.style.top = "0", e.style.left = "0", e.style.width = "100%", e.style.height = "100%"), document.body.appendChild(e);
+    e.src = n, e.style.zIndex = "100", e.style.border = "0", e.style.overflowY = "auto";
+    const h = window.matchMedia("(min-width: 1024px)").matches;
+    e.style.position = "fixed", e.style.bottom = "10px", e.style.right = "10px", e.style.width = i ? `${i}` : "360px", e.style.height = c ? `${c}` : "700px", h || (e.style.position = "fixed", e.style.top = "0", e.style.left = "0", e.style.width = "100%", e.style.height = "100%"), document.body.appendChild(e);
   } else
     return console.error("Account and Assistant id's are required."), null;
 };
 window.dacoidSDK = {
-  init: h
+  init: m
 };
